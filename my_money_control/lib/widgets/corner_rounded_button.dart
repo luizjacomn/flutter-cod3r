6bba71bdf59136c306325efc192b91114d1b3472
Luiz Jacó <luizjacomn@gmail.com>
2020-04-03 23:35:51 -0300

    Criando corner_rounded_button

---
+++ my_money_control/lib/widgets/corner_rounded_button.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:my_money_control/utils/styles.dart';
+
+enum CornerRounded { Left, Right }
+
+class CornerRoundedButton extends StatelessWidget {
+  final CornerRounded cornerRounded;
+  final VoidCallback onPressed;
+  final Color backgroundColor;
+  final String label;
+  final IconData icon;
+  final double cornerRadius;
+
+  const CornerRoundedButton({
+    Key key,
+    @required this.cornerRounded,
+    @required this.onPressed,
+    @required this.backgroundColor,
+    @required this.label,
+    @required this.icon,
+    this.cornerRadius: 24.0,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return InkWell(
+      onTap: onPressed,
+      child: Container(
+        height: 50.0,
+        width: size.width * 1 / 3,
+        decoration: BoxDecoration(
+          color: backgroundColor,
+          borderRadius: cornerRounded == CornerRounded.Left
+              ? BorderRadius.only(topLeft: Radius.circular(cornerRadius))
+              : BorderRadius.only(topRight: Radius.circular(cornerRadius)),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              label.toUpperCase(),
+              style: boldTextStyle.copyWith(
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(width: 8.0),
+            Icon(
+              icon,
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
