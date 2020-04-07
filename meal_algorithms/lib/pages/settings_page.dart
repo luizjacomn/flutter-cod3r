f8c4fb84d796f29843b72ab95203a85d57a2370e
Luiz Jacó <luizjacomn@gmail.com>
2020-04-07 20:21:34 -0300

    Finalizando página de configurações

--- meal_algorithms/lib/pages/settings_page.dart
+++ meal_algorithms/lib/pages/settings_page.dart
@@ -1,9 +1,51 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
+import 'package:meal_algorithms/models/settings.dart';
 
 import 'package:meal_algorithms/widgets/meals_drawer.dart';
 
-class SettingsPage extends StatelessWidget {
+class SettingsPage extends StatefulWidget {
   static const String route = '/settings';
+  final Function(Settings) onSettingsChange;
+  final Settings settings;
+
+  const SettingsPage(this.settings, this.onSettingsChange);
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  Settings settings;
+
+  Widget _switch(
+    String title,
+    String subtitle,
+    bool value,
+    Function(bool) onChange,
+  ) {
+    return ListTile(
+      title: Text(title),
+      subtitle: Text(subtitle),
+      trailing: CupertinoSwitch(
+        activeColor: Theme.of(context).accentColor,
+        value: value,
+        onChanged: (bool) {
+          onChange(bool);
+
+          widget.onSettingsChange(settings);
+        },
+      ),
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    this.settings = widget.settings;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,9 +53,41 @@ class SettingsPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('CONFIGURAÇÕES'),
       ),
-      drawer: MealsDrawer(),
-      body: Center(
-        child: const Text('CONFIGURAÇÕES'),
+      drawer: MealsDrawer(SettingsPage.route),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                _switch(
+                  'Sem glutén',
+                  'Exibe apenas refeições sem glutén',
+                  settings.glutenFree,
+                  (value) => setState(() => settings.glutenFree = value),
+                ),
+                _switch(
+                  'Sem lactose',
+                  'Exibe apenas refeições sem lactose',
+                  settings.lactoseFree,
+                  (value) => setState(() => settings.lactoseFree = value),
+                ),
+                _switch(
+                  'Veganas',
+                  'Exibe apenas refeições veganas',
+                  settings.vegan,
+                  (value) => setState(() => settings.vegan = value),
+                ),
+                _switch(
+                  'Vegetarianas',
+                  'Exibe apenas refeições vegetarianas',
+                  settings.vegetarian,
+                  (value) => setState(() => settings.vegetarian = value),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
