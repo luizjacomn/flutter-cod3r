e03fc215883dddf8a1aec7b34c905470138150fa
Luiz Jacó <luizjacomn@gmail.com>
2020-04-08 18:20:59 -0300

    Adicionando dependência

--- calculadora/lib/main.dart
+++ calculadora/lib/main.dart
@@ -1,16 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+import 'package:calculadora/pages/calculadora.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Container(),
-    );
-  }
-}
+void main() => runApp(Calculadora());
+
