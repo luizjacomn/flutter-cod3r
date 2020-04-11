e4d1ce0e59cf2bc66ae865a75443f21822845143
Luiz Jacó <luizjacomn@gmail.com>
2020-04-11 10:04:42 -0300

    Criando construtores nomeados

--- dart_fundamentos/poo/data.dart
+++ dart_fundamentos/poo/data.dart
@@ -5,11 +5,42 @@ class Data {
 
   Data([this.dia = 1, this.mes = 1, this.ano = 1970]);
 
+  Data.com({this.dia: 1, this.mes: 1, this.ano: 1970});
+
+  Data.ultimoDiaDoAno(this.ano) {
+    this.dia = 31;
+    this.mes = 12;
+  }
+
+  Data.ultimoDiaDoMes(this.mes, this.ano) {
+    switch (this.mes) {
+      case 1:
+      case 3:
+      case 5:
+      case 7:
+      case 8:
+      case 10:
+      case 12:
+        this.dia = 31;
+        break;
+      case 2:
+        this.dia = this.ano % 4 == 0 ? 29 : 28;
+        break;
+      case 4:
+      case 6:
+      case 9:
+      case 11:
+      default:
+        this.dia = 30;
+    }
+  }
+
   @override
   String toString() {
     final d = dia.toString().padLeft(2, '0');
     final m = mes.toString().padLeft(2, '0');
-    return '$d/$m/$ano';
+    final a = ano.toString().padLeft(4, '0');
+    return '$d/$m/$a';
   }
 }
 
@@ -29,4 +60,13 @@ main() {
   print(dataOpcionalAnoPadrao);
   print(dataOpcionalMesAnoPadrao);
   print(dataOpcionalDiaMesAnoPadrao);
+
+  print(Data.com(dia: 25, mes: 12, ano: 0));
+
+  print(Data.ultimoDiaDoAno(2019));
+
+  print('============== MESES =================');
+  for (var mes = 1; mes <= 12; mes++) {
+    print(Data.ultimoDiaDoMes(mes, 2020));
+  }
 }
